1cb9ecfdca7948f79ded6a792018eddd2a1b2ac4
Amelia Bertozzi-Villa <bertozzivill@gmail.com>
2018-09-22 14:21:26 -0700

    answer questions from Aparna/Swathi

--- analyze_prepost.r
+++ analyze_prepost.r
@@ -14,8 +14,11 @@
 ## 9/3 7-7:30
 ## 9/13 5:45-6:45
 ## 9/15 8-9:15
+## 9/22 12:15-
 ## -----------------------------------------------------------------------------------------------------
 
+library(Hmisc)
+library(plyr)
 library(data.table)
 library(ggplot2)
 
@@ -34,64 +37,110 @@ data[, event.name:=tolower(gsub(" Quiz", "", event.name))]
 
 # todo: standardize column naming
 # todo: check male/female #s in dataset compared to reported above 
-data <- dcast(data, user.id + question.count + question + correct_answer ~ event.name, value.var=c("value", "timestamp"))
+data <- dcast(data, user.id + question.count + question + correct.answer ~ event.name, value.var=c("value", "timestamp"))
+
+setnames(data, c("value_pre", "value_post", "timestamp_pre", "timestamp_post"),
+         c("value.pre", "value.post", "timestamp.pre", "timestamp.post"))
 
 # remove questions greater than 14
 data <- data[question.count<=14]
 
-# check numbers: compare reported with dataset
-# total students in dataset:
-length(unique(data$user.id))
-
 # count by date
-data[, date:= ifelse(is.na(timestamp_pre), format(as.Date(timestamp_post), "%d-%m"), format(as.Date(timestamp_pre), "%d-%m"))]
-
-counts <- unique(data[, list(user.id, date)])
-counts[, count:=.N, by="date"]
+data[, date:= ifelse(is.na(timestamp.pre), format(as.Date(timestamp.post), "%d-%m"), format(as.Date(timestamp.pre), "%d-%m"))]
 
 # only keep data from three reported dates: July 30, August 1, August 4
-# data <- data[date %in% c("30-07", "01-08", "04-08")]
-## don't add up-- does it matter? 
+data <- data[date %in% c("30-07", "01-08", "04-08")]
+data[, school:= mapvalues(date, c("30-07", "01-08", "04-08"), c("Sunshine Academy", "Kendriya Vidyala Tambaram", "St Vincent"))]
 
-complete <- data[!is.na(value_post) & !is.na(value_pre)]
+# question: how many people don't have post tests? are they mostly from one date/school?
+no_post <- data[is.na(value.post)]
+no_post[, .N/14, by="school"]
+
+
+###---- Assessment of pre-test alone, by gender -----------------------------------
+# adjust "Testicles" to "Testicle" and "Fallopian Tube" to "Fallopian Tubes"
+data[value.pre=="Testicles", value.pre:="Testicle"]
+data[value.post=="Testicles", value.post:="Testicle"]
+data[value.pre=="Fallopian Tube", value.pre:="Fallopian Tubes"]
+data[value.post=="Fallopian Tube", value.post:="Fallopian Tubes"]
+
+# demographics data
+demog <- fread(file.path(main_dir, "demog.csv"))
+
+data <- merge(data, demog[, list(user.id, sex=capitalize(self.report.sex))], by="user.id", all.x=T)
+
+pre <- copy(data)
+pre[, c("value.post", "timestamp.post"):=NULL]
+
+# 8 questions refer to female organs, 6 to male
+pre[, organ.sex:= ifelse(correct.answer %in% c("Ovary", "Uterus", "Vagina", "Fallopian Tubes", "Urethra Female", "Anus Female"),
+                            "Female", "Male")]
+
+# scores-- overall and by sex of anatomy referenced
+scores_pre <- pre[, list(score.total=100*sum(value.pre==correct.answer)/14), by=list(user.id, sex, school)]
+
+pre[, total:=100*sum(value.pre==correct.answer)/14, by=user.id]
+pre[, female.organs:=100*sum(value.pre==correct.answer & organ.sex=="Female")/8, by=user.id]
+pre[, male.organs:=100*sum(value.pre==correct.answer & organ.sex=="Male")/6, by=user.id]
+
+scores_pre <- unique(pre[, list(user.id, sex, school, total, female.organs, male.organs)])
+scores_pre <- melt(scores_pre, id.vars = c("user.id", "sex", "school"), value.name="score")
+
+ggplot(scores_pre, aes(x=variable, y=score)) +
+  geom_boxplot(aes(fill=sex), alpha=0.5) +
+  facet_grid(school~.) +
+  labs(title="Scores: Overall and by Sex of Question",
+       x="",
+       y="Score")
+
+### ------ Assessment of pre and post test in comparison to each other -----------------------------
+
+# 'complete' here meaning "having responses to both pre and post test" 
+complete <- data[!is.na(value.post) & !is.na(value.pre)]
 
 # time elapsed between pre and post
-complete[, elapsed_minutes:= as.numeric(difftime(timestamp_post, timestamp_pre, units="mins"))]
-ggplot(complete[elapsed_minutes<500], aes(x=elapsed_minutes)) + geom_density() + geom_vline(xintercept=4, color="red")
+complete[, elapsed.minutes:= as.numeric(difftime(timestamp.post, timestamp.pre, units="mins"))]
+ggplot(complete[elapsed.minutes<500], aes(x=elapsed.minutes)) + geom_density() + geom_vline(xintercept=4, color="red")
 
 # based on this, drop everyone with less than 4 or more than 50 minutes between tests
-invalid_users <- unique(complete[elapsed_minutes<4 | elapsed_minutes>50]$user.id)
+invalid_users <- unique(complete[elapsed.minutes<4 | elapsed.minutes>50]$user.id)
 complete <- complete[!(user.id %in% invalid_users)]
 
-# adjust "Testicles" to "Testicle" and "Fallopian Tube" to "Fallopian Tubes"
-complete[value_pre=="Testicles", value_pre:="Testicle"]
-complete[value_post=="Testicles", value_post:="Testicle"]
-complete[value_pre=="Fallopian Tube", value_pre:="Fallopian Tubes"]
-complete[value_post=="Fallopian Tube", value_post:="Fallopian Tubes"]
+
+# question: what is the pre/post time gap for those who answer entirely "not sure" to post-tests? is there a difference 
+# between schools? 
+notsure_post <- complete[value.post=="not sure"]
+notsure_post[, count:=.N, by=user.id]
+notsure_post[question.count==1 & count==14, .N, by="school"]
+notsure_post <- notsure_post[count==14]
+
+ggplot(notsure_post, aes(x=question)) +
+  geom_bar(aes(fill=value.pre))
+
 
 # calculate scores
-complete[, c("elapsed_minutes"):=NULL]
-complete[, score_pre:=100*sum(value_pre==correct_answer)/14, by="user.id"]
-complete[, score_post:=100*sum(value_post==correct_answer)/14, by="user.id"]
-complete[, score_gain:=score_post-score_pre]
+complete[, c("elapsed.minutes"):=NULL]
+complete[, score.pre:=100*sum(value.pre==correct.answer)/14, by="user.id"]
+complete[, score.post:=100*sum(value.post==correct.answer)/14, by="user.id"]
+complete[, score.gain:=score.post-score.pre]
 
-scores <- unique(complete[, list(user.id, score_pre, score_post, score_gain)])
+scores <- unique(complete[, list(user.id, score.pre, score.post, score.gain)])
 
-ggplot(scores, aes(x=score_pre, y=score_post))+ 
+ggplot(scores, aes(x=score.pre, y=score.post))+ 
   geom_jitter(alpha=0.5) +
   geom_abline()
 
-ggplot(scores, aes(x=score_gain)) +
+ggplot(scores, aes(x=score.gain)) +
   geom_density() + 
   geom_vline(xintercept=0, color="red")
 
 ## todo on these: assess prob and cumulative prob of getting an answer right by random chance, given the format of the quiz
 
 # question-by-question analysis of responses
-complete <- complete[value_post!=""] # drop one buggy line for user 544
-complete_long <- melt(complete, id.vars = c("user.id", "question.count", "question", "correct_answer"), 
-                      measure.vars=c("value_pre", "value_post"))
-complete_long[, variable:=factor(variable, levels=c("value_pre", "value_post"), labels=c("Pre", "Post"))]
+complete <- complete[value.post!=""] # drop one buggy line for user 544
+complete_long <- melt(complete, id.vars = c("user.id", "question.count", "question", "correct.answer"), 
+                      measure.vars=c("value.pre", "value.post"))
+complete_long[, variable:=factor(variable, levels=c("value.pre", "value.post"), labels=c("Pre", "Post"))]
 complete_long[, value:=factor(value, levels=rev(c("Anus Female", "Anus Male", "Bladder Male", "Fallopian Tubes", 
                                               "Ovary", "Penis", "Testicle", "Urethra Female", "Uterus", "Vagina", "not sure")))]
 
@@ -108,11 +157,11 @@ ggplot(complete_long, aes(x=variable)) +
   labs(x="", y="")
 
 threecolor <- gg_color_hue(3)
-complete_long[, short_answer:= ifelse(value==correct_answer, "Correct", 
+complete_long[, short.answer:= ifelse(value==correct.answer, "Correct", 
                                       ifelse(value=="not sure", "Not Sure", "Incorrect"))]
-complete_long[, short_answer:=factor(short_answer, levels=c("Incorrect", "Not Sure", "Correct"))]
+complete_long[, short.answer:=factor(short.answer, levels=c("Incorrect", "Not Sure", "Correct"))]
 ggplot(complete_long, aes(x=variable)) +
-  geom_bar(aes(fill=short_answer)) + 
+  geom_bar(aes(fill=short.answer)) + 
   scale_fill_manual(values=c(threecolor[1], threecolor[3], threecolor[2])) + 
   # coord_flip() +
   facet_wrap(~question) +
